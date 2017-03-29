07330e687ecd79b4bb7b3faf0aca7704aa44461e
Liam Lawrence <and3212@gmail.com>
2017-03-29 11:32:56 -0400

    Create archInstaller.sh

---
+++ archInstaller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author: Liam Lawrence
+# Date: 3.29.17
+# Main file that will make installing arch easier
+
+# Sets up the computer
+./setup.sh
+
+# Formats partitions and installs base arch system
+./install.sh
