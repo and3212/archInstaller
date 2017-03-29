e3edb06e294af9ec02baae13e49d590b9b6e6509
Liam Lawrence <and3212@gmail.com>
2017-03-29 16:15:08 -0400

    Added two more files

---
+++ config.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Author: Liam Lawrence
+# Date: 3.29.17
+# Configuration for Arch Linux
+
+TIMEZONE=""
+
+# Switch to the newly installed base system
+arch-chroot /mnt /bin/bash
+
+# Sets your language to en_US.UTF-8 UTF-8
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+
+# Generate new locales
+locale-gen
+
+# Tells the computer what language to use and creates a file
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+clear
+ls /usr/share/zoneinfo
+echo "===================="
+read -p "Pick a timezone to use"
+
+# Sets root password
+echo -e "$ROOTPASS\n$ROOTPASS" | passwd
+
+# Sets up network
+echo "#<ip-address>	<hostname.domain.org>	<hostname>" > /etc/hosts
+echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
+echo "::1		localhost.localdomain	localhost" >> /etc/hosts
+echo "127.0.1.1	$HOST.localdomain		$HOST" >> /etc/hosts
+echo "$HOST" >> /etc/hostname
+systemctl enable dhcpcd
+
+# Sets up our Network Manager Applet
+pacman -S --noconfirm network-manager-applet
+systemctl enable NetworkManager.service
+
+while true; do
+	clear
+	ip link
+
+	read -p "Do you have an ethernet device [y/N]: " -n 1 -r
+	REPLY={REPLY,,}
+	if [ "$REPLY" != "n" ]; then
+		read -p "Enter the name of your ethernet device: " ethvar
+		read -p "Is this the name of your ethernet device - $ethvar - [y/N]: " -n 1 -r
+		REPLY={REPLY,,}
+		if [ "$REPLY" != "n" ]; then
+			continue
+		else
+			systemctl disable $ethvar.service
+		fi
+	fi
+	
+	clear
+	ip link
+
+	read -p "Enter the name of your wifi device: " wifivar
+	read -p "Is this the name of your wifi device - $wifivar - [y/N]: " -n 1 -r
+	REPLY={REPLY,,}
+	if [ "$REPLY" != "n" ]; then
+		continue
+	else
+		systemctl disable netctl-auto@$wifivar.service
+	fi
+done
+
+# Installs sound drivers
+pacman -S --noconfirm alsa-utils
+
+# Installs Yaourt
+echo "[archlinuxfr]" >> /etc/pacman.conf
+echo "SigLevel = Never" >> /etc/pacman.conf
+echo "Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
+pacman -Sy --noconfirm yaourt
+
+# Installs GRUB
+pacman -S --noconfirm grub os-prober
+grub-install $ARCHFILE
+grub-mkconfig -o /boot/grub/grub.cfg
---
+++ postInstall.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Author: Liam Lawrence
+# Date: 3.29.17
+# Post installation that configures the rest of the system
+
+pacman -Syu --noconfirm
+
+# Sets up mirrorlist
+echo "[...]" >> /etc/pacman.conf
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+# Edits our mirrorlist
+
+pacman --noconfirm -Syy
+
+# Sets up the users account
+useradd -m -g users -G wheel,storage,power -s /bin/bash $USER
+echo -e "$USERPASS\n$USERPASS" | passwd $USER
+pacman -S --noconfirm sudo
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+echo "$USER ALL=(ALL) ALL" >> /etc/sudoers
+
+# Installs xorg
+pacman -S --noconfirm xorg xorg-server
+
+
+# Options for the xorg menu
+OPTIONS=(1 "Xfce"
+         2 "Budgie"
+		 3 "GNOME"
+         4 "Cinnamon"
+		 5 "KDE"
+		 6 "Mate"
+		 7 "Deepin"
+		 8 "Enlightenment"
+		 9 "LXDE"
+		 10 "lxqt")
+
+TITLE="Xorg - Desktop Managers"
+MENU="Choose a desktop manager to install:"
+
+
+CHOICE=$(dialog --clear \
+	        --backtitle "$BACKTITLE" \
+	        --title "$TITLE" \
+	        --menu "$MENU" \
+	        $HEIGHT $WIDTH $CHOICE_HEIGHT \
+	        "${OPTIONS[@]}" \
+	        2>&1 >/dev/tty)
+
+clear
+
+# Lets you choose your favorite version of X
+case $CHOICE in
+	1)
+		pacman -S --noconfirm xfce4 xfce4-goodies
+		;;
+	2)
+		pacman -S --noconfirm budgie-desktop			
+		;;
+	3)
+		pacman -S --noconfirm gnome gnome-extra
+		;;
+	4)
+		pacman -S --noconfirm Cinnamon nemo-filerroller
+		;;
+	5)
+		pacman -S --noconfirm plasma
+		;;
+	6)
+		pacman -S --noconfirm mate mate-extra
+		;;
+	7)	
+		pacman -S --noconfirm deepin deepin-extra
+		;;
+	8)
+		pacman -S --noconfirm enlightenment
+		;;
+	9)
+		pacman -S --noconfirm lxde
+		;;
+	10)
+		pacman -S --noconfirm lxqt
+		;;
+esac
+
+# Finishes install
+pacman -S --noconfirm lxdm
+systemctl enable lxdm
+reboot
